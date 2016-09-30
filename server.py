24486ab8fa4a67f5219f2fb507a320eec144a84f
João Gehlen <joaogehlen91@gmail.com>
2016-09-29 21:02:21 -0300

    Update server.py

--- server.py
+++ server.py
@@ -7,18 +7,37 @@ global cartas, valores, tipos
 cartas  = []
 tipos   = ['Paus', 'Espadas', 'Copas', 'Ouros']
 valores = [str(i) for i in range(1,11)] + ['J', 'Q', 'K']
+players = []
+
 
 @get('/gerar_cartas')
 def gera_cartas():
-	for i in tipos:
-		for j in valores:
-			cartas.append(Carta(i, j))
+	if not cartas: # gerar cartas somente uma vez
+		for i in tipos:
+			for j in valores:
+				cartas.append(Carta(i, j))
+	random.shuffle(cartas)
 	redirect('/')
+
 
 @get('/embaralhar_cartas')
 def embaralhar_cartas():
 	random.shuffle(cartas)
 	redirect('/')
+
+
+@get('/get_players/<porta>')
+def get_players(porta):
+	players.append(porta)
+	print(players)
+
+
+@get('/get_cartas')
+def get_cartas():
+	c1 = cartas.pop(0)
+	c2 = cartas.pop(0)
+	return str([c1, c2])
+	
 
 
 @get('/')
