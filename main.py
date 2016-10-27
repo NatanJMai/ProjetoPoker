b178062f3645168739223ac49bb41761fa3f047c
Joao Gehlen <joaogehlen91@gmail.com>
2016-10-27 17:59:55 -0200

    script controlador para testes

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import time
+from sys import argv
+
+def run():
+    os.system("python3 server.py 8080 &")
+    time.sleep(2)
+    os.system("python3 client.py 8010 8080 &")
+    time.sleep(2)
+    os.system("python3 client.py 8020 8080 &")
+    time.sleep(2)
+    os.system("python3 client.py 8030 8080 &")
+    time.sleep(2)
+    os.system("python3 client.py 8040 8080 &")
+
+
+def stop():
+    os.system("pkill -f server.py &")
+    os.system("pkill -f client.py &")
+
+
+if argv[1] == 'r':
+    run()
+    
+if argv[1] == 's':
+    stop()
--- script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-python3 server.py 8080 & python3 client.py 8070 8080 & python3 client.py 8060 8080
